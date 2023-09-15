2b9fd134c4511c91ee10592448f57e8c15bf0d76
Jimmy Ko <ko.jimmy09@gmail.com>
2023-09-15 14:39:06 -0400

    render list of featured events on HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import EventList from "../components/event-list";
+import { getFeaturedEvents } from "../dummy-data";
+
 export default function HomePage() {
+    const featuredEvents = getFeaturedEvents();
+
     return (
         <div>
             <h1>Home Page</h1>
+            <EventList events={featuredEvents} />
         </div>
     );
 }
