550f850c422308b678721b8f6a2d02c30b1ded71
Jimmy Ko <ko.jimmy09@gmail.com>
2023-09-15 17:44:28 -0400

    add more styles and icons to EventItem

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,7 +1,10 @@
-import Link from "next/link";
-import classes from "./event-item.module.css"
+import classes from "./event-item.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
-export default function EventItem({event}) {
+export default function EventItem({ event }) {
     const { id, title, location, date, image } = event;
     const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
         day: "numeric",
@@ -17,23 +20,28 @@ export default function EventItem({event}) {
                 <div className={classes.summary}>
                     <h2>{title}</h2>
                     <div className={classes.date}>
+                        <DateIcon />
                         <time>{humanReadableDate}</time>
                     </div>
                     <div className={classes.address}>
+                        <AddressIcon />
                         <address>{formattedAddress}</address>
                     </div>
                 </div>
                 <div className={classes.actions}>
-                    <Link
-                        href={{
+                    <Button
+                        link={{
                             pathname: "/events/[eventid]",
                             query: {
                                 eventid: id,
                             },
                         }}
                     >
-                        Explore Event
-                    </Link>
+                        <span>Explore Event</span>
+                        <span className={classes.icon}>
+                            <ArrowRightIcon />
+                        </span>
+                    </Button>
                 </div>
             </div>
         </li>
