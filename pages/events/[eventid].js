c68f628016e28e286787338f4f047e2b2a4c216c
Jimmy Ko <ko.jimmy09@gmail.com>
2023-09-15 20:13:26 -0400

    render event details on EventDetailPage with Max's sample components

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,7 +1,30 @@
-export default function EventPage() {
+import { getEventById } from "../../dummy-data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+export default function EventDetailPage() {
+    const { query } = useRouter();
+    const event = getEventById(query.eventid);
+
+    if (!event) {
+        return <h3>There's no event that goes by that name</h3>;
+    }
+
     return (
-        <div>
-            <h1>One Event</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                alt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     );
 }
