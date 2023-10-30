9d587cd80c39be83a175f2e594752289889490b5
Jimmy Ko <ko.jimmy09@gmail.com>
2023-10-30 00:10:32 -0400

    add sales page with data from Firebase, useEffect, and useSWR

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,10 +1,47 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export default function LastSalesPage(props) {
     const [isLoading, setIsLoading] = useState(false);
     const [salesData, setSalesData] = useState();
 
+    const { data, error } = useSWR(
+        "https://react-refresher-ff088-default-rtdb.firebaseio.com/sales.json",
+        (url) => {
+            return fetch(url)
+                .then((res) => res.json())
+                // .then((parsedData) => {
+                //     const transformedData = [];
+                //     for (const sale in parsedData) {
+                //         transformedData.push({
+                //             id: sale,
+                //             username: parsedData[sale].username,
+                //             volume: parsedData[sale].volume,
+                //         });
+                //     }
+                //     return transformedData;
+                // });
+        }
+    );
+
     useEffect(() => {
+        // FOR USE WITH useSWR
+
+        // console.log("in useEffect:", data)
+        // const transformedData = [];
+        // for (const sale in data) {
+        //     transformedData.push({
+        //         id: sale,
+        //         username: data[sale].username,
+        //         volume: data[sale].volume,
+        //     });
+        // }
+        // setSalesData(transformedData);
+
+        // FOR USE WITHOUT useSWR
+
+        console.log("in useEffect")
+
         setIsLoading(true);
 
         fetch(
@@ -12,21 +49,23 @@ export default function LastSalesPage(props) {
         )
             .then((response) => response.json())
             .then((data) => {
-                console.log(data)
                 const transformedData = [];
                 for (const sale in data) {
-                    console.log(sale)
                     transformedData.push({
                         id: sale,
                         username: data[sale].username,
                         volume: data[sale].volume,
                     });
                 }
-                console.log(transformedData)
                 setSalesData(transformedData);
                 setIsLoading(false);
             });
     }, []);
+
+    if (error) {
+        console.log(error);
+        return <p>Failed to Load</p>;
+    }
 
     if (isLoading) {
         return <p>Loading...</p>;
@@ -40,7 +79,7 @@ export default function LastSalesPage(props) {
         <ul>
             {salesData.map((sale) => {
                 return (
-                    <li>
+                    <li key={sale.id}>
                         <p>Salesman: {sale.username}</p>
                         <p>Sales: {sale.volume}</p>
                     </li>
