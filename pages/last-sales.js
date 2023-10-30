41334ab0846238d6304dfd7dc21b562ee4d6eff6
Jimmy Ko <ko.jimmy09@gmail.com>
2023-10-30 13:15:16 -0400

    build last-sales with SSG and client-side rendering

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -2,72 +2,68 @@ import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 export default function LastSalesPage(props) {
-    const [isLoading, setIsLoading] = useState(false);
-    const [salesData, setSalesData] = useState();
+    // const [isLoading, setIsLoading] = useState(false);
+    const [salesData, setSalesData] = useState(props.sales);
 
-    const { data, error } = useSWR(
+    const { data, error, isLoading } = useSWR(
         "https://react-refresher-ff088-default-rtdb.firebaseio.com/sales.json",
         (url) => {
-            return fetch(url)
-                .then((res) => res.json())
-                // .then((parsedData) => {
-                //     const transformedData = [];
-                //     for (const sale in parsedData) {
-                //         transformedData.push({
-                //             id: sale,
-                //             username: parsedData[sale].username,
-                //             volume: parsedData[sale].volume,
-                //         });
-                //     }
-                //     return transformedData;
-                // });
+            return fetch(url).then((res) => res.json());
+            // .then((parsedData) => {
+            //     const transformedData = [];
+            //     for (const sale in parsedData) {
+            //         transformedData.push({
+            //             id: sale,
+            //             username: parsedData[sale].username,
+            //             volume: parsedData[sale].volume,
+            //         });
+            //     }
+            //     return transformedData;
+            // });
         }
     );
 
     useEffect(() => {
         // FOR USE WITH useSWR
-
-        // console.log("in useEffect:", data)
-        // const transformedData = [];
-        // for (const sale in data) {
-        //     transformedData.push({
-        //         id: sale,
-        //         username: data[sale].username,
-        //         volume: data[sale].volume,
-        //     });
-        // }
-        // setSalesData(transformedData);
+        console.log("in useEffect:", data)
+        const transformedData = [];
+        for (const sale in data) {
+            transformedData.push({
+                id: sale,
+                username: data[sale].username,
+                volume: data[sale].volume,
+            });
+        }
+        setSalesData(transformedData);
 
         // FOR USE WITHOUT useSWR
 
-        console.log("in useEffect")
-
-        setIsLoading(true);
-
-        fetch(
-            "https://react-refresher-ff088-default-rtdb.firebaseio.com/sales.json"
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                const transformedData = [];
-                for (const sale in data) {
-                    transformedData.push({
-                        id: sale,
-                        username: data[sale].username,
-                        volume: data[sale].volume,
-                    });
-                }
-                setSalesData(transformedData);
-                setIsLoading(false);
-            });
-    }, []);
+        // console.log("in useEffect")
+        // setIsLoading(true);
+        // fetch(
+        //     "https://react-refresher-ff088-default-rtdb.firebaseio.com/sales.json"
+        // )
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         const transformedData = [];
+        //         for (const sale in data) {
+        //             transformedData.push({
+        //                 id: sale,
+        //                 username: data[sale].username,
+        //                 volume: data[sale].volume,
+        //             });
+        //         }
+        //         setSalesData(transformedData);
+        //         setIsLoading(false);
+        //     });
+    }, [data]);
 
     if (error) {
         console.log(error);
         return <p>Failed to Load</p>;
     }
 
-    if (isLoading) {
+    if (isLoading && !salesData) {
         return <p>Loading...</p>;
     }
 
@@ -87,4 +83,28 @@ export default function LastSalesPage(props) {
             })}
         </ul>
     );
+}
+
+export async function getStaticProps() {
+    const response = await fetch(
+        "https://react-refresher-ff088-default-rtdb.firebaseio.com/sales.json"
+    );
+
+    const data = await response.json();
+
+    const transformedData = [];
+
+    for (const sale in data) {
+        transformedData.push({
+            id: sale,
+            username: data[sale].username,
+            volume: data[sale].volume,
+        });
+    }
+    return {
+        props: {
+            sales: transformedData,
+        },
+        revalidate: 5,
+    };
 }
